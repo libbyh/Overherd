846ce99c0a3fcfbb3b7c26bd5efe45114303ec9d
kevnam <kevnam@gmail.com>
2010-01-18 16:07:32 -0500

    test

---
+++ src/ui/TreeMap.java
@@ -0,0 +1,15 @@
+
+
+package ui;
+
+import prefuse.*;
+
+/**
+ * The main TreeMap display class for the forum data.
+ * 
+ * @author <a href="http://kevinnam.com">kevin nam</a> 
+ */
+
+public class TreeMap extends Display {
+	
+}
