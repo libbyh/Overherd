43dc1741e4a70bae4e3c611f39c8e2178ed8f6bf
kevnam <kevnam@gmail.com>
2010-01-20 21:07:44 -0500

    kevin

---
+++ src/ui/MainUI.java
@@ -0,0 +1,17 @@
+package ui;
+
+/**
+ * @author kevin nam
+ * @version 0.1
+ * 
+ * The main user interfaces.
+ * It has three components.  1) the treemap display 2) viewer for content 3) controller
+ */
+
+import javax.swing.*;
+
+public class MainUI extends JFrame {
+	super("o v e r h e r d | v i s u a l i z a t i o n");
+	
+	
+}
