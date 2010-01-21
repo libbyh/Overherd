6d5e4e29dac86ffb1489633b4cbf1252cfa6e95c
kevnam <kevnam@gmail.com>
2010-01-20 21:12:37 -0500

    kevin

--- src/ui/MainUI.java
+++ src/ui/MainUI.java
@@ -11,7 +11,33 @@ package ui;
 import javax.swing.*;
 
 public class MainUI extends JFrame {
-	super("o v e r h e r d | v i s u a l i z a t i o n");
+	private int width=1000;
+	private int height=900;
 	
+	public MainUI(){
+		super ("o v e r h e r d | v i s u a l i z a t i o n");
+		createAndShowGUI();
+	}
 	
+	public MainUI(int width, int height){
+		super ("o v e r h e r d | v i s u a l i z a t i o n");
+		this.width=width;
+		this.height=height;
+		createAndShowGUI();
+	}
+	
+	public void createAndShowGUI(){
+		this.setSize(width, height);
+		
+		this.setVisible(true);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String args[]){
+		javax.swing.SwingUtilities.invokeLater(new Runnable(){
+			public void run(){
+				MainUI ui=new MainUI();
+			}
+		});
+	}
 }
