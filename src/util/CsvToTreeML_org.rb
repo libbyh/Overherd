d7c4cef3ad81eb4c0e7dc312f322576611fcc08d
kevnam <kevnam@gmail.com>
2010-08-19 16:22:12 -0400

    ruby

--- src/util/CsvToTreeML_org.rb
+++ src/util/CsvToTreeML_org.rb
@@ -24,6 +24,39 @@ forums_array=FasterCSV.read(forums_filename, :headers => true)
 topics_array=FasterCSV.read(topics_filename, :headers => true)
 messages_array=FasterCSV.read(messages_filename, :headers => true)
 
+
+def format_date(str)
+  temp_date=str
+ temp_date=temp_date.gsub(/\//,"-")
+ temp_date=temp_date.sub("JAN","1")
+ temp_date=temp_date.sub("FEB","2")
+ temp_date= temp_date.sub(/MAR/,"3")
+ temp_date=temp_date.sub(/APR/,"4")
+ temp_date=temp_date.sub(/MAY/,"5")
+ temp_date=temp_date.sub(/JUN/,"6")
+ temp_date=temp_date.sub(/JUL/,"7")
+ temp_date=temp_date.sub(/AUG/,"8")
+ temp_date=temp_date.sub(/SEP/,"9")
+ temp_date=temp_date.sub(/OCT/,"10")
+ temp_date=temp_date.sub(/NOV/,"11")
+ temp_date=temp_date.sub(/DEC/,"12")
+  return temp_date
+end
+
+#str needs to be in the format of  "17-11-08 23:51:00"
+def get_epoch_time(str)
+    year=str[7..8]
+    month=str[3..4]
+    day=str[0..1]
+    hour=str[9..10]
+    min=str[12..13]
+    sec=str[15..16]
+    
+    return Time.local(year,month,day,hour,min,sec).tv_sec
+    
+end
+
+
 #parent_branch is the branch to add an element to
 def addMessageElement(parent_branch, data_array)
   parent=parent_branch.parent
@@ -35,7 +68,12 @@ def addMessageElement(parent_branch, data_array)
   puts "adding " + data_array[5]
   branch2.add_element("attribute","name"=>"message_title", "value"=>data_array[5])
   puts "adding " + data_array[6]
-  branch2.add_element("attribute","name"=>"message_date", "value"=>data_array[6])
+  temp_date=format_date(data_array[6])
+ 
+  branch2.add_element("attribute","name"=>"message_date", "value"=>temp_date)
+  
+  temp_epoch=get_epoch_time(temp_date)
+  branch2.add_element("attribute","name"=>"epoch_seconds", "value"=>temp_epoch)
   branch2.add_element("attribute","name"=>"message_author","value"=>data_array[10])
   branch2.add_element("attribute","name"=>"type", "value"=>"message")
   branch2.add_element("attribute","name"=>"forum_id","value"=>data_array[1])
@@ -51,15 +89,16 @@ def addMessageElement(parent_branch, data_array)
   end
   branch2.add_element("attribute","name"=>"message_body","value"=>temp)
   temp=data_array[8]
-  if temp==nil
-   temp=""
-  else
-   temp=data_array[8].strip_html
-  end
+ # if temp==nil
+ #  temp=""
+ # else
+ #  temp=data_array[8].strip_html
+ # end
   branch2.add_element("attribute","name"=>"message_parent", "value"=>temp)
   
   return branch2
 end
+
 
 
 #although it looks like messages.csv has messages grouped together properly, can't count on that.  
@@ -68,7 +107,9 @@ end
 #I am declaring all the ids of type String snice I don't know how big they can get. If I know the upper
 # limit, I can change them to Integer or something else.
 
-# supported date types for TreeML are: Integer, Long, Float, Real, String, Date
+# supported date types for TreeML are: Integer, Long, Float, Real, String, Date (though I am having a trouble with Date)
+
+# I am wondering if we can have just id, author, ... for all three types, since I am distinguishing them by 'type'
 doc=Document.new
 doc.add_element("tree")
 doc.root.add_element("declarations")
@@ -77,11 +118,11 @@ doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"forum_t
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"forum_id", "type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"forum_short","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"forum_extended","type"=>"String"})
-doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"forum_date","type"=>"Date"})
+doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"forum_date","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"forum_length","type"=>"Integer"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"topic_id","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"topic_title","type"=>"String"})
-doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"topic_date","type"=>"Date"})
+doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"topic_date","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"topic_creator","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"topic_short","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"topic_extended","type"=>"String"})
@@ -91,16 +132,13 @@ doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"message
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"message_author","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"message_body","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"message_length","type"=>"Integer"})
-doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"message_date","type"=>"Date"})
+doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"message_date","type"=>"String"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"message_parent","type"=>"String"})
+doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"epoch_seconds","type"=>"Long"})
 doc.root.elements["declarations"].add_element("attributeDecl", {"name"=>"type","type"=>"String"}) #type indicates forum, topic, or message
 
 
-#experimental - big hash has a node id and link to dom tree, for a fast lookup
-big_hash=Hash.new
-
 branch1=doc.root.add_element("branch")
-
 
 #add forums
 forums_array.each{|forum|
@@ -108,7 +146,13 @@ forums_array.each{|forum|
  branch2.add_element("attribute", "name"=>"name","value"=>forum[2])
  branch2.add_element("attribute","name"=>"forum_title", "value"=>forum[2])
  branch2.add_element("attribute","name"=>"forum_id","value"=>forum[1])
- branch2.add_element("attribute","name"=>"forum_date", "value"=>forum[3])
+
+ temp_date=format_date(forum[3])
+# puts "date: " + temp_date
+ branch2.add_element("attribute","name"=>"forum_date", "value"=>temp_date)
+ temp_epoch=get_epoch_time(temp_date)
+ branch2.add_element("attribute","name"=>"epoch_seconds", "value"=>temp_epoch)
+ 
  branch2.add_element("attribute","name"=>"forum_short", "value"=>forum[5])
 
  branch2.add_element("attribute","name"=>"type", "value"=>"forum")
@@ -121,11 +165,6 @@ forums_array.each{|forum|
  end
 # puts temp_extended
  branch2.add_element("attribute","name"=>"forum_extended", "value"=>forum[6])
-
-  #add it to big_hash so its childrent can have a fast lookup
-  big_hash[forum[1]]=branch2
-  puts "forum id: " + forum[1]
-  puts big_hash[forum[1]]
 }
 
 
@@ -140,7 +179,12 @@ topics_array.each {|topic|
   puts "Adding topic " + topic[2] + " " + topic[3]
   branch4.add_element("attribute","name"=>"topic_title", "value"=>topic[3])
   branch4.add_element("attribute","name"=>"topic_id","value"=>topic[2])
-  branch4.add_element("attribute","name"=>"topic_date","value"=>topic[4])
+
+  temp_date=format_date(topic[4])
+  branch4.add_element("attribute","name"=>"topic_date","value"=>temp_date)
+  temp_epoch=get_epoch_time(temp_date)
+  branch4.add_element("attribute","name"=>"epoch_seconds", "value"=>temp_epoch)
+  
   branch4.add_element("attribute","name"=>"topic_creator","value"=>topic[5])
   branch4.add_element("attribute","name"=>"topic_short","value"=>topic[6])
   branch4.add_element("attribute","name"=>"topic_extended","value"=>topic[7])
@@ -149,7 +193,7 @@ topics_array.each {|topic|
 }
 
 #added=[]
-not_inserted=Hash.new
+not_inserted=[]
 countline=0
 
 #I know this is messy and bad, but for now..
@@ -160,10 +204,10 @@ countline=0
 
 temp_count=0; 
 messages_array.each{ |message|
-  temp_count=temp_count+1
-  if (temp_count==255)
-    break    
-  end
+#  temp_count=temp_count+1
+#  if (temp_count==10)
+#    break    
+ # end
   puts "-- doing line " + countline.to_s() + ", message " + message[3]
   reply_to=message[8]
   not_inserted_this_time=false
@@ -171,8 +215,7 @@ messages_array.each{ |message|
   if(reply_to==nil)
     #if reply_to is empty, means top node
     puts "top node " + message[4].to_s() + " found.  Being added."
-  #  branch=doc.root.elements["//attribute[@value=" + topic_id+"]"]
-    branch=big_hash[topic_id]
+    branch=doc.root.elements["//attribute[@value=" + topic_id+"]"]
     branch2=addMessageElement(branch,message)
  #   puts message
  #   branch2=branch.add_element("branch")
@@ -180,7 +223,7 @@ messages_array.each{ |message|
  #   branch2.add_element("attribute","name"=>"message_id","value"=>message[4])
  #   branch2.add_element("attribute","name"=>"message_body","value"=>message[11])
     #since a new node is added, check not_inserted whether their parent is the newly added one
-    temp_not_inserted=Array.new(not_inserted.keys)
+    temp_not_inserted=Array.new(not_inserted)
     temp_not_inserted.each{|line|
       reply_to=messages_array[line][8]
       puts "cheking if reply_to ("+reply_to.to_s()+") == "+message[4].to_s()
@@ -205,8 +248,8 @@ messages_array.each{ |message|
    # if(!added.include?(reply_to))
       #parent not found
       puts "parent (" + reply_to.to_s() + ") not found.  Line "+ countline.to_s() +" added to not_inserted list." 
-      if(!not_inserted.has_key?(countline))
-        not_inserted[countline]=branch
+      if(!not_inserted.include?(countline))
+        not_inserted.push(countline)
         not_inserted_this_time=true
       end
     else
@@ -223,7 +266,7 @@ messages_array.each{ |message|
       
       #since a new node is added to the tree, check whether this is the parent of not_inserted
       #bug probably coming from using the same array for iterating and deleting.  Use different array
-      temp_not_inserted=Array.new(not_inserted.keys)
+      temp_not_inserted=Array.new(not_inserted)
       temp_not_inserted.each{|line|
         reply_to=messages_array[line][8]
         puts "cheking if reply_to ("+reply_to.to_s()+") == "+message[4].to_s()
@@ -239,8 +282,8 @@ messages_array.each{ |message|
       }
     end
     
-    num_to_check=not_inserted.keys.length
-    puts "not_inserted len: " + not_inserted.keys.length.to_s()
+    num_to_check=not_inserted.length
+    puts "not_inserted len: " + not_inserted.length.to_s()
     checked_count=0
     
   end
@@ -249,9 +292,9 @@ messages_array.each{ |message|
 
 
 checkcount=5
-while(not_inserted.keys.length>0 and checkcount>0)
+while(not_inserted.length>0 and checkcount>0)
   puts "check for the remaining nodes...count " + checkcount.to_s()
-  temp_not_inserted=Array.new(not_inserted.keys)
+  temp_not_inserted=Array.new(not_inserted)
   temp_not_inserted.each{|line|
     reply_to=messages_array[line][8]
     puts "cheking for reply_to ("+reply_to.to_s()+")"
@@ -270,9 +313,9 @@ while(not_inserted.keys.length>0 and checkcount>0)
   checkcount=-checkcount
 end
 #check if there's anything left in the not_inserted.  There shouldn't be.
-if(not_inserted.keys.length>0)
+if(not_inserted.length>0)
   puts "One or more nodes without a parent have been found!  Their lines are:"
-  not_inserted.keys.each{|num|
+  not_inserted.each{|num|
     puts num 
   }
 end
