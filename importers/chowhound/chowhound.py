e4ea6c23d187ebb0368694640020dfe3ec424941
Ruoran Wang <ruoranwang@gmail.com>
2010-11-15 18:25:30 -0600

    some prompt & content fix

--- importers/chowhound/chowhound.py
+++ importers/chowhound/chowhound.py
@@ -25,16 +25,16 @@ def fetch(url,method = "GET"):
             return data
     domain = SCRAPING_DOMAIN_RE.findall(url)[0]
     if SCRAPING_DOMAINS.has_key(domain):
-		last_scraped = SCRAPING_DOMAINS[domain]
-		elapsed = now - last_scraped
-	if elapsed < SCRAPING_REQUEST_STAGGER:
-	    wait_period = (SCRAPING_REQUEST_STAGGER - elapsed) / 1000
-	    time.sleep(wait_period)
+        last_scraped = SCRAPING_DOMAINS[domain]
+        elapsed = now - last_scraped
+        if elapsed < SCRAPING_REQUEST_STAGGER:
+            wait_period = (SCRAPING_REQUEST_STAGGER - elapsed) / 1000
+            time.sleep(wait_period)
     SCRAPING_DOMAINS[domain] = time.time()
     data = SCRAPING_CONN.request(url, method)
     SCRAPING_CACHE[key] = (data, now)
     end = time.time() #testing
-    ftime.append(end-start)
+    ftime.append(end-start) #testing
     return data
 
 def goodString(input):
@@ -45,13 +45,13 @@ def goodString(input):
     i = 0
     j = len(string)-1
     while(True):
-	if string[i].isalpha() or string[i].isdigit():
-	    break
-	i=i+1
+        if string[i].isalpha() or string[i].isdigit():
+            break
+        i=i+1
     while(True):
-	if string[j].isalpha() or string[j].isdigit():
-	    break
-	j=j-1
+        if string[j].isalpha() or string[j].isdigit():
+            break
+        j=j-1
     return string[i:j+1]
 
 def goodDigit(input):
@@ -59,25 +59,31 @@ def goodDigit(input):
     i = 0
     j = len(string)-1
     while(True):
-	if string[i].isdigit():
-	    break
-	i=i+1
+        if string[i].isdigit():
+            break
+        i=i+1
     while(True):
-	if string[j].isdigit():
-	    break
-	j=j-1
+        if string[j].isdigit():
+            break
+        j=j-1
     return string[i:j+1]
 
 def getPostContent(soup): # post_soup ok, good_reply soup ok.
     """ input a soup, will return content string """
+    """ 
     post = soup.findAll('div', 'post_body', limit = 1)[0]
     content = ""
-    for x in range(1, len(post.contents) - 1):
-	p = ""
-	for item in post.contents[x].contents:
-	    p = p + str(item)
+    for x in range(1, len(post.contents)):
+        p = ""
+        for item in post.contents[x].contents:
+            p = p + str(item)
         content = content + p
-	"""str(root_post.contents[x].contents[0])"""
+    """ 
+    post = soup.find('div', 'post_body')
+    content_list = post.findAll(text = True)
+    content = ""
+    for item in content_list:
+        content = content + item
     return content
     
 def getADT(soup, level = 1): #soup is good reply
@@ -85,15 +91,15 @@ def getADT(soup, level = 1): #soup is good reply
     A author, D data, T time
     """
     if level == 0:
-		adt = soup.findAll('p', 'vcard author', limit = 1)[0]
-		author = str(adt.strong.contents[0].string)
+        adt = soup.findAll('p', 'vcard author', limit = 1)[0]
+        author = str(adt.strong.contents[0].string)
     else: # if it is a reply
-		adt = soup.findAll('p', 'vcard author', limit = 1)[0]
-	if len(adt.strong.contents) == 1:
-	    author = str(adt.strong.contents[0].string)
-	else:
-	    author = str(adt.strong.contents[1].string)
-    # above code exists as the vcard block for post and reply are different 	
+        adt = soup.findAll('p', 'vcard author', limit = 1)[0]
+        if len(adt.strong.contents) == 1:
+            author = str(adt.strong.contents[0].string)
+        else:
+            author = str(adt.strong.contents[1].string)
+    # above code exists as the vcard block for post and reply are different     
     date = goodString( adt.strong.nextSibling.string[5:] ) # data is the same. 
     return author, date
 
@@ -102,10 +108,10 @@ def getID(soup):
     idString = soup.findAll('div', 'post_body', limit = 1)[0].attrs[1][1]
     the_id = goodDigit(idString)
     if the_id in id_list:
-	return -1
+        return -1
     else:
-	id_list.append(the_id)
-	return the_id
+        id_list.append(the_id)
+        return the_id
     
 def getTitle(soup):
     """ only root_post could use this method"""
@@ -121,52 +127,52 @@ def add(parent_e, post_soup, level = 1):
     """
     start = time.time() #testing
     if level == 0: #post
-	post_soup_e = SubElement(parent_e, "post")
+        post_soup_e = SubElement(parent_e, "post")
         title = getTitle(post_soup)
-	SubElement(post_soup_e, "title").text = title
-	the_id = getID(post_soup)
-	if the_id == -1: #in id list
-	    return
-	SubElement(post_soup_e, "id").text = the_id
-	
+        SubElement(post_soup_e, "title").text = title
+        the_id = getID(post_soup)
+        if the_id == -1: #in id list
+            return
+        SubElement(post_soup_e, "id").text = the_id
+    
         (author, date) = getADT(post_soup, 0)
         SubElement(post_soup_e, "author").text = author
         SubElement(post_soup_e, "date").text = date
-	content = getPostContent(post_soup)
+        content = getPostContent(post_soup)
         SubElement(post_soup_e, "content").text = content
-	SubElement(post_soup_e, "char_count").text = str(len(content))
-	word_count = len(content.split(" "))
-	SubElement(post_soup_e, "word_count").text = str(word_count)
+        SubElement(post_soup_e, "char_count").text = str(len(content))
+        word_count = len(content.split(" "))
+        SubElement(post_soup_e, "word_count").text = str(word_count)
     else:
-	post_soup_e = SubElement(parent_e, "reply")
-	
-	the_id = getID(post_soup)
-	if the_id == -1: #in id list
-	    return
-	SubElement(post_soup_e, "id").text = the_id
-	
+        post_soup_e = SubElement(parent_e, "reply")
+    
+        the_id = getID(post_soup)
+        if the_id == -1: #in id list
+            return
+        SubElement(post_soup_e, "id").text = the_id
+    
         (author, date) = getADT(post_soup)
-	SubElement(post_soup_e, "author").text = author
-	SubElement(post_soup_e, "date").text = date
-	content = getPostContent(post_soup)
-	SubElement(post_soup_e, "content").text = content
-	SubElement(post_soup_e, "char_count").text = str(len(content))
-	word_count = len(content.split(" "))
-	SubElement(post_soup_e, "word_count").text = str(word_count)    
+        SubElement(post_soup_e, "author").text = author
+        SubElement(post_soup_e, "date").text = date
+        content = getPostContent(post_soup)
+        SubElement(post_soup_e, "content").text = content
+    SubElement(post_soup_e, "char_count").text = str(len(content))
+    word_count = len(content.split(" "))
+    SubElement(post_soup_e, "word_count").text = str(word_count)    
     
     
     if haveReply(post_soup) != 0: # haveReply?  == have <li>...</li> ?
-	sub_soup = haveReply(post_soup, num = 0)[0]
-	num = len( sub_soup.findNextSiblings('li') )
-	#if num == 0:
-	#    add(post_soup_e, sub_soup)
-	#else:
-	for i in range(0, num + 1):
-	    add(post_soup_e, sub_soup)
-	    if len(sub_soup.findNextSiblings('li', limit = 2)) != 0:
-		sub_soup = sub_soup.findNextSiblings('li', limit = 2)[0]
-	#add(post_soup_e, sub_soup)
-	
+        sub_soup = haveReply(post_soup, num = 0)[0]
+        num = len( sub_soup.findNextSiblings('li') )
+        #if num == 0:
+        #    add(post_soup_e, sub_soup)
+        #else:
+        for i in range(0, num + 1):
+            add(post_soup_e, sub_soup)
+            if len(sub_soup.findNextSiblings('li', limit = 2)) != 0:
+             sub_soup = sub_soup.findNextSiblings('li', limit = 2)[0]
+        #add(post_soup_e, sub_soup)
+    
     end = time.time()
     global atime
     atime.append(end - start)
@@ -179,18 +185,18 @@ def haveReply(soup, num = 1): # only good_reply soup ok
     replies_raw = [tag for tag in soup.findAll('li')]
     replies = []   
     for i in range(0, len(replies_raw)):
-	if replies_raw[i].ol != None:
-	    replies.append(replies_raw[i])
-	    
+        if replies_raw[i].ol != None:
+            replies.append(replies_raw[i])
+        
     end = time.time() #testing
     global rtime
     rtime.append(end - start)
     if num ==1:
-	return len(replies)
+        return len(replies)
     elif num == 0:
-	return replies
+        return replies
     elif num == 2:
-	return len(replies), replies
+        return len(replies), replies
 
 def main():
     global id_list
@@ -198,31 +204,37 @@ def main():
     all_post_soup_list = []  
     
 
-    print "which board you want to scrap?"
-    print "Chicago?(y/n)"
-    #flag = input()
-    #if flag != 'y' or 'Y' or 'yes' or 'Yes':
-	#print "bye!"
-	#return
-    url = u"http://chowhound.chow.com/boards/4"
+    print "which board you want to scrap(input a number)?"
+    board_number = unicode((raw_input()))
+    url = u"http://chowhound.chow.com/boards/"
+    url = url + board_number
+    print "testing?(y/n)"
+    testing = raw_input()
+    
+    first_topic = 0 
+    if testing == "y":
+        first_topic = 40
+    
     start = time.time()
     page = fetch(url,"GET")
     soup = BeautifulSoup(page[1])
     
     firstPart = soup.findAll("td","post_title")
     for item in firstPart:
-	urls.append(unicode(item.findChildren()[1]['href']))
-   
-    f3 = open("tree6.xml","w")
+        urls.append(unicode(item.findChildren()[1]['href']))
+  
+    tree_name = "tree" + board_number + ".xml" 
+    f3 = open(tree_name, "w")
     overherd = Element("overherd")
     middle = time.time()
-    for i in range(30, len(urls)):
-	page = fetch(urls[i], "GET")
-	soup = BeautifulSoup(page[1])
-	add(overherd, soup, 0)
+    for i in range(first_topic, len(urls)):
+        page = fetch(urls[i], "GET")
+        soup = BeautifulSoup(page[1])
+        add(overherd, soup, 0)
 
     
     ElementTree(overherd).write(f3)
+
     end = time.time()
     print middle-start
     print end-middle
@@ -230,16 +242,16 @@ def main():
     global ftime
     sum1 = 0
     for item in ftime:
-	sum1 = sum1 + item
+        sum1 = sum1 + item
     global rtime
     sum2 = 0
     for item in rtime:
-	sum2 = sum2 + item
+        sum2 = sum2 + item
     print "have reply time ",sum2
-    print "fetch time ",sum1	
+    print "fetch time ",sum1    
     sum2=0
     for item in atime:
-	sum2 = sum2 +item
+        sum2 = sum2 +item
     print "add time ",sum2
     
     #f2.close()
