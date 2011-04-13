1e770eb03a946f50a5a47cf19baec1a3c70fb50d
Ruoran <ruoranwang@gmail.com>
2011-04-12 21:07:04 -0500

    importer update with a sample xml file

--- importers/chowhound/chowhound.py
+++ importers/chowhound/chowhound.py
@@ -13,7 +13,6 @@ SCRAPING_CACHE_FOR = 60 * 15
 SCRAPING_REQUEST_STAGGER = 1100
 SCRAPING_CACHE = {} 
 
-
 ftime=[]
 
 def fetch(url,method = "GET"):
@@ -101,6 +100,10 @@ def getADT(soup): #soup is good reply
     # above, author done 
     datetime = goodString( adt.strong.nextSibling.string[5:] )
     date = datetime[8:12]+"-"+datetime[0:3]+"-"+datetime[4:6]
+    the_string = datetime[10:12] + " " + datetime[0:3] + " " + datetime[4:6]
+    # the_string = "YY MMM DD"
+    the_date_time = time.strptime(the_string, "%y %b %d")
+    epoch_seconds = str(int(time.mktime(the_date_time)))
     month_dic = {"Jan":"01", "Feb":"02", "Mar":"03", "Apr":"04", \
             "May":"05", "Jun":"06", "Jul":"07", "Aug":"08", "Sep":"09", \
             "Oct":"10", "Nov":"11", "Dec":"12"}
@@ -111,8 +114,9 @@ def getADT(soup): #soup is good reply
     if datetime[-2:] == "PM" and hh != "12":
         hh = str(int(hh)+12)
     mmss = ":"+datetime[16:18]+":00"
-    time = hh + mmss
-    return author, date, time
+    the_time = hh + mmss
+    the_datetime = date + " " + the_time
+    return author, the_datetime, epoch_seconds
 
 def getID(soup):
     idString = soup.findAll('div', 'post_body', limit = 1)[0].attrs[1][1]
@@ -124,82 +128,67 @@ def getTitle(soup):
     title = soup.findAll('h1', 'title entry-title', limit = 1)[0].string
     return fixup(goodString(title))
     
-
 atime = []
+
+topic_soup_e = None
 def add(parent_e, post_soup, level = 1): 
     """ parent_post is a Element, post is Soup instance 
-    level 0: root post
-    level 1: Default reply
+    level 0: topic  
+    level 1: Default reply(message)
     """
     start = time.time() #testing
-    if level == 0: #post
-        post_soup_e = SubElement(parent_e, "post")
-        title = getTitle(post_soup)
-        SubElement(post_soup_e, "title").text = title
-        #the_id = getID(post_soup)
-        #if the_id == -1: #in id list
-        #    return
-        #SubElement(post_soup_e, "id").text = the_id
     
-        #(author, date, post_time) = getADT(post_soup)
-        #SubElement(post_soup_e, "author").text = author
-        #SubElement(post_soup_e, "date").text = date
-        #SubElement(post_soup_e, "time").text = post_time
-        #content = getPostContent(post_soup)
-        #SubElement(post_soup_e, "content").text = content
-        #SubElement(post_soup_e, "char_count").text = str(len(content))
-        #word_count = len(content.split(" "))
-        #SubElement(post_soup_e, "word_count").text = str(word_count)
+    post_soup_e = SubElement(parent_e, "branch")
+    
+    global topic_soup_e 
+    if level == 0: #post
+        topic_soup_e = post_soup_e
+        title = getTitle(post_soup)
+        """    
+        SubElement(post_soup_e, "attribute", name = "topic_title", value = title)
+        SubElement(post_soup_e, "attribute", name = "name", value = title)
+        the_type = "topic"
+        the_content ="topic_extended"
+        """
+        #SubElement(post_soup_e, "attribute", name = "message_title", value = title)
+        SubElement(post_soup_e, "attribute", name = "name", value = title)
+        the_type = "message"
+        the_content ="message_body"
+
     else:
-        post_soup_e = SubElement(parent_e, "reply")
+        #SubElement(post_soup_e, "attribute", name = "message_title", value = "message no title")
+        SubElement(post_soup_e, "attribute", name = "name", value = "message not name")
+        the_type = "message"
+        the_content ="message_body"
+    
+    SubElement(post_soup_e, "attribute", name = "type", value = the_type)
     
     the_id = getID(post_soup)
-        #if the_id == -1: #in id list
-        #    return
-    SubElement(post_soup_e, "id").text = the_id
+    SubElement(post_soup_e, "attribute", name = the_type + "_id", value = the_id)
     
-    (author, date, post_time) = getADT(post_soup)
-    SubElement(post_soup_e, "author").text = author
-    SubElement(post_soup_e, "date").text = date
-    SubElement(post_soup_e, "time").text = post_time
+    (author, date_time, epoch_seconds) = getADT(post_soup)
+    SubElement(post_soup_e, "attribute", name = "author", value = author)
+    SubElement(post_soup_e, "attribute", name = the_type + "_date", value = date_time)
+    #SubElement(post_soup_e, "attribute", name = "time", value = post_time)
     content = getPostContent(post_soup)
-    SubElement(post_soup_e, "content").text = content
-    SubElement(post_soup_e, "char_count").text = str(len(content))
+    SubElement(post_soup_e, "attribute", name = the_content, value = content)
+
+    #SubElement(post_soup_e, "attribute", name = "chars", value = str(len(content)))
     word_count = len(content.split(" "))
-    SubElement(post_soup_e, "word_count").text = str(word_count)    
-    
+    #SubElement(post_soup_e, "attribute", name = "words", value = str(word_count))   
+    SubElement(post_soup_e, "attribute", name = "epoch_seconds", value = epoch_seconds)
+
     num,sub_soup_list =  haveReply(post_soup, num = 2)  
     if num !=0:
         sub_soup = sub_soup_list[0]
         num = len( sub_soup.findNextSiblings('li') )
         for i in range(0, num + 1):
-            add(post_soup_e, sub_soup)
+            add(topic_soup_e, sub_soup)
             if len(sub_soup.findNextSiblings('li', limit = 2)) != 0:
                 sub_soup = sub_soup.findNextSiblings('li', limit = 2)[0]
-
-    # 
-    """
-    if haveReply(post_soup) != 0: # haveReply?  == have <li>...</li> ?
-        sub_soup = haveReply(post_soup, num = 0)[0]
-        num = len( sub_soup.findNextSiblings('li') )
-        #if num == 0:
-        #    add(post_soup_e, sub_soup)
-        #else:
-        for i in range(0, num + 1):
-            add(post_soup_e, sub_soup)
-            if len(sub_soup.findNextSiblings('li', limit = 2)) != 0:
-                sub_soup = sub_soup.findNextSiblings('li', limit = 2)[0]
-        #add(post_soup_e, sub_soup)
-    """
-    #
-
-
-    end = time.time()
-    global atime
-    atime.append(end - start)
-    return
 
 rtime = [] #testing
+
 def haveReply(soup, num = 1): # only good_reply soup ok
     start = time.time() #testing
     replies_raw = [tag for tag in soup.findAll('li')]
@@ -212,32 +201,10 @@ def haveReply(soup, num = 1): # only good_reply soup ok
     global rtime
     rtime.append(end - start)
     return len(replies), replies
-#old haveReply
-'''
-def haveReply(soup, num = 1): # only good_reply soup ok
-    start = time.time() #testing
-    replies_raw = [tag for tag in soup.findAll('li')]
-    replies = []   
-    for i in range(0, len(replies_raw)):
-        if replies_raw[i].ol != None:
-            replies.append(replies_raw[i])
-        
-    end = time.time() #testing
-    global rtime
-    rtime.append(end - start)
-    if num == 1:
-        return len(replies)
-    elif num == 0:
-        return replies
-    elif num == 2:
-        return len(replies), replies
-'''
-#old haveReply
 
 def main():
     urls=[]
-    all_post_soup_list = []  
-    
+    all_post_soup_list = []      
 
     print "which board you want to scrape(input a number)?"
     board_number = unicode((raw_input()))
@@ -248,7 +215,7 @@ def main():
     
     first_topic = 0 
     if testing == "y":
-        first_topic = 48
+        first_topic = 45
     
     start = time.time()
     page = fetch(url,"GET")
@@ -259,18 +226,51 @@ def main():
         urls.append(unicode(item.findChildren()[1]['href']))
   
     tree_name = "tree" + board_number + ".xml" 
-    f3 = open(tree_name, "w")
-    overherd = Element("overherd")
+    file = open(tree_name, "w")
+    overherd = Element("tree")
+    dec = SubElement(overherd, "declarations")
+    
+    SubElement(dec, "attributeDecl", name = "name", type = "String")
+    SubElement(dec, "attributeDecl", name = "forum_title", type = "String")
+    SubElement(dec, "attributeDecl", name = "forum_short", type = "String")
+    SubElement(dec, "attributeDecl", name = "topic_title", type = "String")
+    SubElement(dec, "attributeDecl", name = "topic_short", type = "String")
+    SubElement(dec, "attributeDecl", name = "topic_id", type = "String")
+    SubElement(dec, "attributeDecl", name = "topic_date", type = "String")
+    SubElement(dec, "attributeDecl", name = "topic_extended", type = "String")
+    SubElement(dec, "attributeDecl", name = "author", type = "String")
+    SubElement(dec, "attributeDecl", name = "time", type = "String")
+    SubElement(dec, "attributeDecl", name = "message_title", type = "String")
+    SubElement(dec, "attributeDecl", name = "message_id", type = "String")
+    SubElement(dec, "attributeDecl", name = "message_body", type = "String")
+    SubElement(dec, "attributeDecl", name = "message_date", type = "String")
+    SubElement(dec, "attributeDecl", name = "chars", type = "String")
+    SubElement(dec, "attributeDecl", name = "words", type = "String") 
+    #SubElement(dec, "attributeDecl", name = "forum", type = "String") 
+    SubElement(dec, "attributeDecl", name = "type", type = "String") 
+    SubElement(dec, "attributeDecl", name = "epoch_seconds", type = "Long") 
+    
+    branch = SubElement(overherd, "branch")
+    
+    forum = SubElement(branch, "branch")
+    SubElement(forum, "attribute", name = "name", value = "Chowhound/Chicago name")
+    SubElement(forum, "attribute", name = "forum_title", value = "Chowhound/Chicago title")
+    SubElement(forum, "attribute", name = "forum_short", value = "Chowhound/Chicago short")
+    SubElement(forum, "attribute", name = "type", value = "forum")
+    SubElement(forum, "attribute", name = "author", value = "Chowhound Author")
+    SubElement(forum, "attribute", name = "epoch_seconds", value = "1226983822")
+    
+   
     for i in range(first_topic, len(urls)):
         page = fetch(urls[i], "GET")
         soup = BeautifulSoup(page[1])
-        add(overherd, soup, 0)
+        add(forum, soup, 0)
 
-     
-    ElementTree(overherd).write(f3)
+    ElementTree(overherd).write(file)
 
     end = time.time()
     print "total time ",end-start
+    ''' 
     global ftime
     sum1 = 0
     for item in ftime:
@@ -285,9 +285,8 @@ def main():
     for item in atime:
         sum1 = sum1 +item
     print "add time ",sum1
-    
-    #f2.close()
-    f3.close()
+    ''' 
+    file.close()
     
     
 if __name__ == "__main__":
